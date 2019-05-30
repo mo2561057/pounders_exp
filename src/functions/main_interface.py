6313d33b393f84b6f52599439c4551533942d22a
mo2561057 <moritz.mendel12@gmail.com>
2019-05-30 13:34:26 +0200

    created architecturde

--- src/functions/main_interface.py
+++ src/functions/main_interface.py
@@ -1,2 +1,22 @@
+"""
+Define the algorithm required.
+
+"""
+import os
+
+import numpy as np
+from petsc4py import PETSc
 
 
+def solve():
+    pass
+
+
+def _prep_args():
+    """
+    This functions specifies relevant containers
+    """
+    pass
+
+def _set_constraints():
+    
